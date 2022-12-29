5b477b40a7a000ff0b89a66d1d293484242d95c1
Widho Faisal <banggatutor@gmail.com>
2022-12-29 23:54:26 +0700

    project selesai

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from './api.js'
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMoviesList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className='Movie-wrapper'>
+          <div className='Movie-title'>{movie.title}</div>
+          <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className='Movie-date'>release: {movie.release_date}</div>
+          <div className='Movie-rate'>{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+    if(q.length>3){
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+      // console.log({query:query})
+    }
+  }
+
+  console.log({ popularMovies: popularMovies })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Widho LK21 Mania</h1>
+
+        <input
+          placeholder='Search Your Movie...'
+          className='Movie-search'
+          onChange={({ target }) => search(target.value)}
+        />
+
+        <div className='Movie-container'>
+          <PopularMoviesList/>
+        </div>
+
       </header>
     </div>
   );
